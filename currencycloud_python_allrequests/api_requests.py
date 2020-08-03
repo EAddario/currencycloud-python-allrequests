12863a2a288f93f696cadf4a81c9b876fa0c40a7
Ed Addario <eaddario@hotmail.com>
2020-08-03 15:14:18 +0100

    Refactor code format

--- currencycloud_python_allrequests/api_requests.py
+++ currencycloud_python_allrequests/api_requests.py
@@ -23,62 +23,73 @@ client = currencycloud.Client(login_id, api_key, environment)
 init(autoreset=True)
 
 current_account = api.current_account(client)
-output("current_account - Account {0} ({1}), Address: {2}, {3}, {4}, {5}".format(current_account.account_name,
-                                                                                 current_account.id,
-                                                                                 current_account.street,
-                                                                                 current_account.city,
-                                                                                 current_account.postal_code,
-                                                                                 current_account.country))
+output("current_account - Account {0} ({1}), Address: {2}, {3}, {4}, {5}".format(
+    current_account.account_name,
+    current_account.id,
+    current_account.street,
+    current_account.city,
+    current_account.postal_code,
+    current_account.country))
 
 find_accounts = api.find_accounts(client, per_page=3)
 for elmt in find_accounts:
     output("find_accounts - Account {0} ({1})".format(current_account.account_name, current_account.id))
 
-create_account = api.create_account(client, account_name="Wirecard Development",
-                                    legal_entity_type="individual",
-                                    street="12 Steward St",
-                                    city="london",
-                                    postal_code="E1 6FQ",
-                                    country="GB",
-                                    api_trading=True,
-                                    online_trading=True,
-                                    phone_trading=True)
-output("create_account - Account {0} ({1}), Address: {2}, {3}, {4}, {5}".format(create_account.account_name,
-                                                                                create_account.id,
-                                                                                create_account.street,
-                                                                                create_account.city,
-                                                                                create_account.postal_code,
-                                                                                create_account.country))
+create_account = api.create_account(
+    client,
+    account_name="Wirecard Development",
+    legal_entity_type="individual",
+    street="12 Steward St",
+    city="london",
+    postal_code="E1 6FQ",
+    country="GB",
+    api_trading=True,
+    online_trading=True,
+    phone_trading=True)
+output("create_account - Account {0} ({1}), Address: {2}, {3}, {4}, {5}".format(
+    create_account.account_name,
+    create_account.id,
+    create_account.street,
+    create_account.city,
+    create_account.postal_code,
+    create_account.country))
 
 update_account = api.update_account(client, resource_id=create_account.id, city="London")
-output("update_account - Account {0} ({1}), Address: {2}, {3}, {4}, {5}".format(update_account.account_name,
-                                                                                update_account.id,
-                                                                                update_account.street,
-                                                                                update_account.city,
-                                                                                update_account.postal_code,
-                                                                                update_account.country))
+output("update_account - Account {0} ({1}), Address: {2}, {3}, {4}, {5}".format(
+    update_account.account_name,
+    update_account.id,
+    update_account.street,
+    update_account.city,
+    update_account.postal_code,
+    update_account.country))
 
 get_account = api.get_account(client, resource_id=update_account.id)
-output("get_account - Account {0} ({1}), Address: {2}, {3}, {4}, {5}".format(get_account.account_name,
-                                                                             get_account.id,
-                                                                             get_account.street,
-                                                                             get_account.city,
-                                                                             get_account.postal_code,
-                                                                             get_account.country))
+output("get_account - Account {0} ({1}), Address: {2}, {3}, {4}, {5}".format(
+    get_account.account_name,
+    get_account.id,
+    get_account.street,
+    get_account.city,
+    get_account.postal_code,
+    get_account.country))
 
 get_payment_charges = api.get_payment_charges(client, resource_id=get_account.id)
-output("get_payment_charges - Charge Id: {0}, Account Id: {1}, Charge Type: {2}, Enabled? {3}, Default? {4}".format(get_payment_charges[0].charge_settings_id,
-                                                                                                                    get_payment_charges[0].account_id,
-                                                                                                                    get_payment_charges[0].charge_type,
-                                                                                                                    get_payment_charges[0].enabled,
-                                                                                                                    get_payment_charges[0].default))
+output("get_payment_charges - Charge Id: {0}, Account Id: {1}, Charge Type: {2}, Enabled? {3}, Default? {4}".format(
+    get_payment_charges[0].charge_settings_id,
+    get_payment_charges[0].account_id,
+    get_payment_charges[0].charge_type,
+    get_payment_charges[0].enabled,
+    get_payment_charges[0].default))
 
-update_payment_charges = api.update_payment_charges(client, account_id=get_payment_charges[0].account_id, resource_id=get_payment_charges[0].charge_settings_id)
-output("update_payment_charges - Charge Id: {0}, Account Id: {1}, Charge Type: {2}, Enabled? {3}, Default? {4}".format(update_payment_charges.charge_settings_id,
-                                                                                                                       update_payment_charges.account_id,
-                                                                                                                       update_payment_charges.charge_type,
-                                                                                                                       update_payment_charges.enabled,
-                                                                                                                       update_payment_charges.default))
+update_payment_charges = api.update_payment_charges(
+    client,
+    account_id=get_payment_charges[0].account_id,
+    resource_id=get_payment_charges[0].charge_settings_id)
+output("update_payment_charges - Charge Id: {0}, Account Id: {1}, Charge Type: {2}, Enabled? {3}, Default? {4}".format(
+    update_payment_charges.charge_settings_id,
+    update_payment_charges.account_id,
+    update_payment_charges.charge_type,
+    update_payment_charges.enabled,
+    update_payment_charges.default))
 
 get_balance = api.get_balance(client, currency='USD')
 output("Your {0} balance is {1}".format(get_balance.currency, get_balance.amount))
