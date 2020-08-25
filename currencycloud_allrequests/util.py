77230d9396f8c940dbd5f0aba4b46a97c0ee54da
Ed Addario <eaddario@hotmail.com>
2020-08-25 15:10:10 +0100

    Add working days calculator

--- currencycloud_allrequests/util.py
+++ currencycloud_allrequests/util.py
@@ -1,3 +1,4 @@
+import datetime
 import random
 import string
 
@@ -16,3 +17,15 @@ def output(msg):
 def random_string(length):
     chars = string.ascii_uppercase
     return ''.join(random.choice(chars) for i in range(length))
+
+
+def add_working_days(from_date, add_days):
+    business_days_to_add = add_days
+    current_date = datetime.datetime.strptime(from_date, "%Y-%m-%dT%H:%M:%S+00:00")
+    while business_days_to_add > 0:
+        current_date += datetime.timedelta(days=1)
+        weekday = current_date.weekday()
+        if weekday >= 5:
+            continue
+        business_days_to_add -= 1
+    return current_date
